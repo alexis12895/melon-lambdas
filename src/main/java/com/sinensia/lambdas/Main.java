7ae46f5110cfaaa9407e35a86c086ee7c4b3a4e2
Alexis Nasimba <alexis12895@gmail.com>
2022-05-18 17:31:22 +0200

    He implementado Streams.

--- src/main/java/com/sinensia/lambdas/Main.java
+++ src/main/java/com/sinensia/lambdas/Main.java
@@ -1,8 +1,8 @@
 package com.sinensia.lambdas;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class Main {
 
@@ -28,23 +28,22 @@ public class Main {
 
         List<Melon> found = Filtrers.filterByWeight(melons, 1300);
         System.out.println("\nLista filtrada por peso: ");
-        for (Melon m: found) {
+        for (Melon m : found) {
             System.out.println(m);
         }
 
         //Con un metodo anonimo, new GacMelonPre...
-        List<Melon> gac = Filtrers.filterMelon(melons,  new GacMelonPredicate());
+        List<Melon> gac = Filtrers.filterMelon(melons, new GacMelonPredicate());
         System.out.println("\nLista filtrada por nombre gac: ");
-        for (Melon m: gac) {
+        for (Melon m : gac) {
             System.out.println(m);
         }
 
-        List<Melon> heavy = Filtrers.filterMelon(melons,  new HeavyMelonPredicate());
+        List<Melon> heavy = Filtrers.filterMelon(melons, new HeavyMelonPredicate());
         System.out.println("\nLista melones pesados: ");
-        for (Melon m: heavy) {
+        for (Melon m : heavy) {
             System.out.println(m);
         }
-
 
         //Directamente hemos creado la clase anonima que implementa MelonPredicate y asi podemos implementarla aqui mismo
         /*
@@ -71,17 +70,50 @@ public class Main {
 
         //Incorporandolo en el mismo for
         System.out.println("Lista melones europeos: ");
-        for(Melon m:  Filtrers.filterMelon(melons, melon -> melon!=null && melon.getOrigin().equalsIgnoreCase("Europe"))) {
+        for (Melon m : Filtrers.filterMelon(melons, melon -> melon != null && melon.getOrigin().equalsIgnoreCase("Europe"))) {
             System.out.println(m);
         }
 
         //Con un metodo de filtrado generico y usar las lambdas
         System.out.println("Lista sandias: ");
-        List<Melon> watermelons = Filtrers.filter(melons, (Melon m)->m.getType().equalsIgnoreCase("watermelon"));
-        for(Melon m: watermelons) {
+        List<Melon> watermelons = Filtrers.filter(melons, (Melon m) -> m.getType().equalsIgnoreCase("watermelon"));
+        for (Melon m : watermelons) {
             System.out.println(m);
         }
 
+
+        //Streams
+        System.out.println("Stream de melones: ");
+        melons.stream()
+                .forEach(melon -> System.out.println(melon));
+
+        //Solamente aquellos melones  con el nombre gac, mediante el filtro, una especie de tuberia
+        System.out.println("Stream de melones filtrado ");
+        melons.stream()
+                .filter(melon -> "gac".equalsIgnoreCase(melon.getType()))
+                .forEach(melon -> System.out.println(melon));
+
+        System.out.println("Primer elemento del Stream ");
+        //Es una encapsulacion de un objeto de tipo arbitrario, para verificar si es nulo
+        Optional<Melon> primero = melons.stream()
+                .findFirst();
+        if (primero.isPresent()) {
+            System.out.println(primero.get());
+        }
+
+        //Si esta presente lo hace, incluye el if de antes
+        primero.ifPresent(melon -> System.out.println(melon));
+
+        //Nos permite poner los pasos en sus lineas
+        melons.stream()
+                .findFirst()
+                .ifPresent(melon -> System.out.println(melon));
+
+        //Puedo realizar operaciones mas complejas con el mapeo de enteros
+        melons.stream()
+                .mapToInt(melon->melon.getWight())
+                .average()
+                .ifPresent(average->System.out.println("El promedio es: "+average));
 
     }
 
